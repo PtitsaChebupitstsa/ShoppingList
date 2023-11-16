a82f4878d41734386739fc0c361bd11b81d29872
Ptitsa_Chebupitstsa <kerugeru@gmail.com>
2023-11-17 01:53:41 +0600

    Upgrade project and add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,15 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation( "androidx.room:room-runtime:2.3.0")
+    implementation ("androidx.room:room-ktx:2.3.0")
+    kapt ("androidx.room:room-compiler:2.3.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

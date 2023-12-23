73b19b18b046aedc9653e7d556fc8c25a17bc27f
Ptitsa_Chebupitstsa <kerugeru@gmail.com>
2023-12-23 23:31:01 +0600

    13.2 UriMatcher

--- app/src/main/java/com/example/shoppinglistca/data/ShopListProvider.kt
+++ app/src/main/java/com/example/shoppinglistca/data/ShopListProvider.kt
@@ -2,11 +2,15 @@ package com.example.shoppinglistca.data
 
 import android.content.ContentProvider
 import android.content.ContentValues
+import android.content.UriMatcher
 import android.database.Cursor
 import android.net.Uri
 import android.util.Log
 
 class ShopListProvider:ContentProvider() {
+    private val uriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+        addURI("com.kerugeru.shoppinglist","shop_items", GET_SHOP_ITEMS_QUERY)
+    }
     override fun onCreate(): Boolean {
         return true
     }
@@ -19,7 +23,13 @@ class ShopListProvider:ContentProvider() {
         selectionArgs: Array<out String>?,
         sortOrder: String?
     ): Cursor? {
-       Log.d("ShopListProvider","query $uri")
+        val code = uriMatcher.match(uri)
+        when(code){
+            GET_SHOP_ITEMS_QUERY->{
+
+            }
+        }
+       Log.d("ShopListProvider","query $uri code $code")
         return null
     }
 
@@ -46,5 +56,9 @@ class ShopListProvider:ContentProvider() {
         selectionArgs: Array<out String>?
     ): Int {
         TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val GET_SHOP_ITEMS_QUERY= 100
     }
 }
